dda842ac6ed027e0e931ab41370ab89a70d9a529
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-06-23 08:48:54 +0200

    Add modalGameStart.js - End of Work Session : 2h45

---
+++ dist/js/modalGameStart.js
@@ -0,0 +1,6 @@
+// MODAL THROW GAME
+function toggleModal(modalId) {
+    let startGame = document.querySelector("#modal-id");
+    startGame.classList.toggle("hidden");
+    startGame.classList.toggle("flex");
+}
