88fdc0c4414ccf6c091ac51131a6871dca6744af
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-06-23 08:42:07 +0200

    Add modalShowRule.js with toogle function - Complete README.md

---
+++ dist/js/modalShowRules.js
@@ -0,0 +1,6 @@
+// MODAL SHOW RULES
+function toggleModalRule(ruleDisplay) {
+    let showRule = document.querySelector("#rule-display");
+    showRule.classList.toggle("hidden");
+    showRule.classList.toggle("flex");
+}
