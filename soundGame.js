3e99baeeac67c6781eebaf7bebe0a1dc15cfeeaa
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-06-26 12:17:23 +0200

    modify files path

--- soundGame.js
+++ soundGame.js
@@ -1,17 +1,17 @@
 // SOME SOUNDS 
 function diceSound() {
-    let soundDice = new Audio('dist/sounds/roll-dice.wav');
+    let soundDice = new Audio('./sounds/roll-dice.wav');
     soundDice.play();
   }
   function winSound() {
-    let winnerSound = new Audio('dist/sounds/winGame.wav');
+    let winnerSound = new Audio('./sounds/winGame.wav');
     winnerSound.play();
   }
   function loseRound(){
-    let looserSound = new Audio('dist/sounds/looseRound.wav');
+    let looserSound = new Audio('./sounds/looseRound.wav');
     looserSound.play();
   }
   function keepSound() {
-    let glingGling = new Audio('dist/sounds/glingGling.mp3')
+    let glingGling = new Audio('./sounds/glingGling.mp3')
     glingGling.play();
   }
